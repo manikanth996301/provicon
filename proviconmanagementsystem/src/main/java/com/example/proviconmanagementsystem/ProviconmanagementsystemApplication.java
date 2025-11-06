8250a23720793c6eba26297a2ffbc889c34b1a0c
Manikanth <manikanthrathna007@gmail.com>
2025-11-06 19:12:50 +0530

    Test commit

--- proviconmanagementsystem/src/main/java/com/example/proviconmanagementsystem/ProviconmanagementsystemApplication.java
+++ proviconmanagementsystem/src/main/java/com/example/proviconmanagementsystem/ProviconmanagementsystemApplication.java
@@ -8,6 +8,7 @@ public class ProviconmanagementsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProviconmanagementsystemApplication.class, args);
+		
 	}
 
 }
